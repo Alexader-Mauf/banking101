78df36c4ee9a07628ecf43257923f99a4b1dde4e
Alexander Mauf <maufalexander@gmail.com>
2020-12-10 13:32:26 +0100

    classes reset(bugfix)

--- mysite/bank/models.py
+++ mysite/bank/models.py
@@ -1,24 +1,39 @@
 from django.db import models
 
+
 # Create your models here.
 class Customer(models.Model):
-    created_at = models.DateTimeField('created_at')
-    updated_at = models.DateTimeField('updated_at')
-    name = models.CharField('Name')
-    first_name = models.CharField('Vorname')
+    first_name = models.CharField(
+        max_length=255
+    )
+    name = models.CharField(
+        max_length=255
+    )
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Account(models.Model):
-    created_at = models.DateTimeField('created_at')
-    updated_at = models.DateTimeField('updated_at')
-    user_id = models.ForeignKey(Customer,on_delete=models.CASCADE(),related_name='Besitzer',related_query_name='Überweisung')
-    balance = models.DecimalField(22,4,'Kontostand')
+    user_id = models.ForeignKey(
+        Customer,
+        on_delete=models.CASCADE,
+        related_name='accounts'
+    )
+    balance = models.DecimalField(max_digits=22,decimal_places=4)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-
-
-class transfer(models.Model):
-    created_at = models.DateTimeField('created_at')
-    updated_at = models.DateTimeField('updated_at')
-    total = models.DecimalField(22,4,"Überwiesener Betrag")
-    from_acc = models.ForeignKey(Customer, on_delete=models.CASCADE(),related_name='Schuldner')
-    to_acc = models.ForeignKey(Customer, on_delete=models.CASCADE(),related_name='Gläubiger')
+class transfers(models.Model):
+    acc_from = models.ForeignKey(
+        Customer,
+        on_delete= models.CASCADE,
+        related_name='user_that_send'
+    )
+    acc_to = models.ForeignKey(
+        Customer,
+        on_delete= models.CASCADE,
+        related_name='user_that_recieved'
+    )
+    total = models.DecimalField(max_digits=22,decimal_places=4)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
