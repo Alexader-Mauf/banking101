7c894641431c2ea45250216d6c3b1b0d3a5b6016
Alexander Mauf <maufalexander@gmail.com>
2020-12-10 10:15:00 +0100

    hochladen von django versuch 2

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alexander-mauf/Documents/Projects/Programieren Trainieren/bankbeispiel/myvenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
